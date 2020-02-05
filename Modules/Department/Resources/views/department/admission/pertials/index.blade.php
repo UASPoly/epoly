aff4da53a471d001e29634a11039c0d12a938547
isahlabbo <isahlabbo22@example.com>
2020-02-05 11:57:30 +0100

    modify the admission views

--- Modules/Department/Resources/views/department/admission/pertials/index.blade.php
+++ Modules/Department/Resources/views/department/admission/pertials/index.blade.php
@@ -19,7 +19,34 @@
 			     			<th></th>
 			     		</tr>
 			     	</thead>
-			     	
+			     	<tbody>
+			     		@foreach(department()->admissions->where('session_id',$session->id) as $admission)
+			     		<tr>
+			     			<td>{{$loop->index+1}}</td>
+			     			<td>
+			     				{{$admission->student->first_name}} 
+			     				{{$admission->student->middle_name}}    
+			     				{{$admission->student->last_name}}
+			     			</td>
+			     			<td>{{$admission->admission_no}}</td>
+			     			<td>{{$admission->student->schedule->name}}</td>
+			     			<td>{{$admission->programme->title}}</td>
+			     			<td>{{$admission->student->phone}}</td>
+			     			<td>{{$admission->student->is_active == 1 ? 'Active' : 'Revoked'}}</td>
+			     			<td>
+			     				<button class="btn btn-danger shadow" onclick="confirm('Are you sure you want to delete this student from the list of students in this department')"><a href="{{route($route['delete'] ?? 'department.admission.delete',['admission_id'=>$admission->id])}}" style="color: white">Delete</a> </i>
+			     				</button>
+
+			     				<button class="btn btn-info shadow">
+			     					<a href="{{route($route['view'] ?? 'department.admission.edit',[$admission->student->id])}}" style="color: white">View</a></i>
+			     				</button>
+
+			     				<button class="btn btn-warning shadow" ><a href="{{route($route['revoke'] ?? 'department.admission.revoke',['admission_id'=>$admission->id])}}" style="color: white" onclick="confirm('Are you sure you want to revoke this student from having access to his account')" class="text text-primary">{{$admission->student->is_active == 1 ? 'Revoke' : 'Activate'}}</a></i>
+			     				</button>
+			     			</td>
+			     		</tr>
+			     		@endforeach
+			     	</tbody>
 			    </table>
 			@else
 			<div class="col-md-2"></div>
