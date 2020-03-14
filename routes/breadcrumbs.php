26296b7e41261f15b331bbf055e27ef993b32903
abusaleem <isahlabbo22@gmail.com>
2020-03-14 15:19:17 +0100

    issue with exam officer module has not been resolved

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../Modules/Admin/Routes/breadcrumbs.php';
 require __DIR__ . '/../Modules/Department/Routes/breadcrumbs.php';
-require __DIR__ . '/../Modules/ExamOfficer/Routes/breadcrumbs.php';
+//require __DIR__ . '/../Modules/ExamOfficer/Routes/breadcrumbs.php';
 require __DIR__ . '/../Modules/College/Routes/breadcrumbs.php';
 require __DIR__ . '/../Modules/Student/Routes/breadcrumbs.php';
 require __DIR__ . '/../Modules/Lecturer/Routes/breadcrumbs.php';
